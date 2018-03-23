e033a12256563166f2fd91b266a91044f9f7b01e
Philippe Recto <precto1285@gmal.com>
2018-03-23 11:55:25 -0700

    created button functions in js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,22 @@ $(document).ready(function () {
     $('.carousel').carousel('methodName', paramName);
 });
 
+$("#belmontBtn").on("dblclick", function(event){
+    console.log("belmont");
+    
+});
+$("#balboaBtn").on("dblclick", function(event){
+    console.log("balboa");
+});
+$("#gaslampBtn").on("dblclick", function(event){
+    console.log("gaslamp");
+});
+$("#delmarBtn").on("dblclick", function(event){
+    console.log("delmar");
+});
+$("#fashionvalleyBtn").on("dblclick", function(event){
+    console.log("fashion valley");
+});
+$("#carlsbadBtn").on("dblclick", function(event){
+    console.log("carslbad");
+});
